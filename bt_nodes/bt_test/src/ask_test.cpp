ad9310bcd81f6e7b2ec14905aadf78d8f285e814
igonzf <igonzf06@estudiantes.unileon.es>
2024-02-21 08:42:46 +0100

    fix bt ask

--- bt_nodes/bt_test/src/ask_test.cpp
+++ bt_nodes/bt_test/src/ask_test.cpp
@@ -16,6 +16,7 @@ int main(int argc, char *argv[]) {
   factory.registerFromPlugin(loader.getOSName("speak_bt_node"));
   factory.registerFromPlugin(loader.getOSName("listen_bt_node"));
   factory.registerFromPlugin(loader.getOSName("askForSomething_bt_node"));
+  factory.registerFromPlugin(loader.getOSName("dialogConfirmation_bt_node"));
   std::string pkgpath = ament_index_cpp::get_package_share_directory("bt_test");
   std::string xml_file = pkgpath + "/bt_xml/ask_test.xml";
 

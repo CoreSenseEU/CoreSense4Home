6d2e842bc3ac6833179b647a15ad7d4b576c4472
Juancams <jc.manzanares.serrano@gmail.com>
2024-02-26 12:01:41 +0100

    Follow implemented

--- bt_nodes/motion/src/motion/navigation/MoveTo.cpp
+++ bt_nodes/motion/src/motion/navigation/MoveTo.cpp
@@ -35,14 +35,15 @@ MoveTo::tick()
     config().blackboard->get(tf_frame, pose_);
     RCLCPP_INFO(node_->get_logger(), "MoveTo ticked");
 
-    if (!create_and_send_goal()) { return BT::NodeStatus::FAILURE;}
-  }
+  RCLCPP_INFO(node_->get_logger(), "Sending goal: x: %f, y: %f, in frame: %s",
+  goal.pose.position.x, goal.pose.position.y,
+      goal.header.frame_id.c_str());
+  goal_.pose = goal;
+  goal_updated_ = true;
 
-  if (!goal_result_available_) {
-    return BT::NodeStatus::RUNNING;
-  }
-
-  return BT::NodeStatus::SUCCESS;
+  std::string pkgpath = ament_index_cpp::get_package_share_directory("bt_test");
+  std::string xml_file = pkgpath + "/bt_xml/moveto.xml";
+  goal_.behavior_tree = xml_file;
 }
 
 void

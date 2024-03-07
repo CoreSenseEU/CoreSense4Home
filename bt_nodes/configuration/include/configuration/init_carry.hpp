2ae298f6e697170dfd004651bda4d67db0ac736f
juan <juan97pena@gmail.com>
2024-03-07 11:38:36 +0100

    init_carry added

---
+++ bt_nodes/configuration/include/configuration/init_carry.hpp
@@ -0,0 +1,53 @@
+// Copyright 2024 Intelligent Robotics Lab - Gentlebots
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef CONFIGURATION__INIT_CARRY_HPP_
+#define CONFIGURATION__INIT_CARRY_HPP_
+
+#include <string>
+#include "behaviortree_cpp_v3/behavior_tree.h"
+#include "behaviortree_cpp_v3/bt_factory.h"
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+
+namespace configuration
+{
+
+class InitCarry : public BT::ActionNodeBase
+{
+public:
+  explicit InitCarry(
+    const std::string & xml_tag_name,
+    const BT::NodeConfiguration & conf);
+
+  void halt();
+  BT::NodeStatus tick();
+
+  static BT::PortsList providedPorts()
+  {
+    return BT::PortsList({});
+  }
+
+private:
+  rclcpp::Node::SharedPtr node_;
+  std::string cam_frame_, home_pose_, offer_pose_;
+  int person_id;
+  geometry_msgs::msg::PoseStamped home_position_;
+
+};
+
+}  // namespace configuration
+
+#endif  // CONFIGURATION__INIT_CARRY_HPP_

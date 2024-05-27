aea8af1c20363e280e00d08f700c9f6ca37454c5
Irene González Fernández <igonzf06@estudiantes.unileon.es>
2024-05-28 01:30:16 +0200

    add is_gesturing bt_node

--- bt_nodes/bt_test/src/find_person_test.cpp
+++ bt_nodes/bt_test/src/find_person_test.cpp
@@ -18,6 +18,7 @@ int main(int argc, char *argv[]) {
   factory.registerFromPlugin(loader.getOSName("is_detected_bt_node"));
   factory.registerFromPlugin(loader.getOSName("pan_bt_node"));
   factory.registerFromPlugin(loader.getOSName("move_to_bt_node"));
+  factory.registerFromPlugin(loader.getOSName("is_gesturing_bt_node"));
   std::string pkgpath = ament_index_cpp::get_package_share_directory("bt_test");
   std::string xml_file = pkgpath + "/bt_xml/find_person_test.xml";
 

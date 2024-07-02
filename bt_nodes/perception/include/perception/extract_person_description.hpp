889558e6cc450c3ecc12121b5aec78b39d7dab04
Irene González Fernández <igonzf06@estudiantes.unileon.es>
2024-07-02 15:16:53 +0200

    add extract_person_description bt_node

---
+++ bt_nodes/perception/include/perception/extract_person_description.hpp
@@ -0,0 +1,67 @@
+// Copyright 2024 Intelligent Robotics Lab - Gentlebots
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef PERCEPTION__EXTRACT_PERSON_DESCRIPTION_HPP_
+#define PERCEPTION__EXTRACT_PERSON_DESCRIPTION_HPP_
+
+#include <tf2/transform_datatypes.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/static_transform_broadcaster.h>
+#include <tf2_ros/transform_listener.h>
+
+#include <algorithm>
+#include <string>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+
+#include "behaviortree_cpp_v3/behavior_tree.h"
+#include "behaviortree_cpp_v3/bt_factory.h"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "perception_system/PerceptionListener.hpp"
+#include "perception_system_interfaces/msg/detection_array.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "tf2_ros/transform_broadcaster.h"
+
+namespace perception {
+
+using pl = perception_system::PerceptionListener;
+
+class ExtractPersonDescription : public BT::ActionNodeBase {
+public:
+  explicit ExtractPersonDescription(const std::string &xml_tag_name,
+                                    const BT::NodeConfiguration &conf);
+
+  void halt();
+  BT::NodeStatus tick();
+
+  static BT::PortsList providedPorts() {
+    return BT::PortsList({BT::InputPort<std::string>("person_id"),
+                          BT::InputPort<std::string>("interest"),
+                          BT::OutputPort<std::string>("description")});
+  }
+
+private:
+  std::shared_ptr<rclcpp_cascade_lifecycle::CascadeLifecycleNode> node_;
+
+  std::string person_id_;
+  std::string interest_;
+  std::string description_;
+
+  std::map<std::string, std::vector<int>> gestures_;
+  std::map<int, std::string> pose_names_;
+};
+
+} // namespace perception
+
+#endif // PERCEPTION__EXTRACT_PERSON_DESCRIPTION_HPP_
---
+++ bt_nodes/perception/src/perception/extract_person_description.cpp
@@ -0,0 +1,116 @@
+// Copyright 2024 Intelligent Robotics Lab - Gentlebots
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "perception/extract_person_description.hpp"
+
+#include <limits>
+#include <string>
+#include <utility>
+
+#include "behaviortree_cpp_v3/behavior_tree.h"
+#include "perception_system/PerceptionUtils.hpp"
+
+namespace perception {
+
+using namespace std::chrono_literals;
+using namespace std::placeholders;
+
+ExtractPersonDescription::ExtractPersonDescription(
+    const std::string &xml_tag_name, const BT::NodeConfiguration &conf)
+    : BT::ActionNodeBase(xml_tag_name, conf), gestures_({
+                                                  {"pointing_right", {0, 1}},
+                                                  {"pointing_left", {3, 4}},
+
+                                                  {"waving", {5, 6, 7}},
+                                                  {"rising_left", {6}},
+                                                  {"rising_left", {6}},
+                                              }),
+      pose_names_({
+          {0, "lying"},
+          {1, "sitting"},
+          {2, "standing"},
+          {-1, "unknown"},
+      }) {
+  config().blackboard->get("node", node_);
+
+  // pl::getInstance(node_)->trigger_transition(lifecycle_msgs::msg::Transition::TRANSITION_CONFIGURE);
+  // pl::getInstance(node_)->trigger_transition(lifecycle_msgs::msg::Transition::TRANSITION_ACTIVATE);
+}
+
+BT::NodeStatus ExtractPersonDescription::tick() {
+
+  getInput("person_id", person_id_);
+
+  pl::getInstance(node_)->set_interest(person_id_, true);
+  pl::getInstance(node_)->update(30);
+
+  RCLCPP_INFO(node_->get_logger(), "[ExtractPersonDescription] Describe %s",
+              person_id_.c_str());
+
+  auto detections = pl::getInstance(node_)->get_by_id(person_id_);
+
+  if (detections.empty()) {
+    RCLCPP_ERROR(node_->get_logger(),
+                 "[ExtractPersonDescription] No detections");
+    return BT::NodeStatus::FAILURE;
+  }
+
+  RCLCPP_DEBUG(node_->get_logger(),
+               "[ExtractPersonDescription] Processing detections...");
+
+  getInput("interest", interest_);
+
+  if (interest_ == "pose") {
+
+    description_ = pose_names_[detections[0].body_pose];
+    RCLCPP_INFO(node_->get_logger(),
+                "[ExtractPersonDescription] The pose of the person is: %s",
+                description_.c_str());
+    setOutput("description", description_);
+
+    return BT::NodeStatus::SUCCESS;
+  }
+
+  if (interest_ == "gesture") {
+    int pointing_direction = detections[0].pointing_direction;
+
+    for (const auto &gesture : gestures_) {
+      const auto &key = gesture.first;
+      const auto &direction = gesture.second;
+
+      if (std::find(direction.begin(), direction.end(), pointing_direction) !=
+          direction.end()) {
+        description_ = key;
+        setOutput("description", description_);
+        RCLCPP_INFO(
+            node_->get_logger(),
+            "[ExtractPersonDescription] The gesture of the person is: %s",
+            description_.c_str());
+        return BT::NodeStatus::SUCCESS;
+      }
+    }
+  }
+
+  return BT::NodeStatus::SUCCESS;
+}
+void ExtractPersonDescription::halt() {
+  RCLCPP_DEBUG(node_->get_logger(), "ExtractPersonDescription halted");
+}
+
+} // namespace perception
+
+BT_REGISTER_NODES(factory) {
+  factory.registerNodeType<perception::ExtractPersonDescription>(
+      "ExtractPersonDescription");
+}

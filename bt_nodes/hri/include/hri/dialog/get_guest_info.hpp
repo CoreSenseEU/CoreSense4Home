4812857d515fe8685a05c40486bb72d91b2e95e4
igonzf <igonzf06@estudiantes.unileon.es>
2025-12-16 13:11:06 +0100

    add store and get kb info

---
+++ bt_nodes/hri/include/hri/dialog/get_guest_info.hpp
@@ -0,0 +1,61 @@
+// Copyright 2024 Intelligent Robotics Lab - Gentlebots
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions andGO2OBJECT
+// limitations under the License.
+
+#ifndef HRI__GEST_GUEST_INFO_HPP_
+#define HRI__GEST_GUEST_INFO_HPP_
+
+#include <chrono>
+#include <functional>
+
+#include "behaviortree_cpp_v3/behavior_tree.h"
+#include "behaviortree_cpp_v3/bt_factory.h"
+#include "std_msgs/msg/string.hpp"
+#include "kb_msgs/srv/query.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_cascade_lifecycle/rclcpp_cascade_lifecycle.hpp"
+#include "hri/bt_service_node.hpp"
+
+namespace dialog
+{
+
+class GetGuestInfo : public hri::BtServiceNode<
+    kb_msgs::srv::Query,
+    rclcpp_cascade_lifecycle::CascadeLifecycleNode>
+{
+public:
+  explicit GetGuestInfo(const std::string & xml_tag_name, const std::string & srv_name,
+    const BT::NodeConfiguration & conf);
+
+  void on_tick() override;
+  void on_result() override;
+
+  static BT::PortsList providedPorts()
+  {
+    return BT::PortsList(
+      {BT::OutputPort<std::string>("guest_name"),
+        BT::OutputPort<std::string>("guest_drink"),
+        BT::OutputPort<std::string>("guest_description"),
+      });
+  }
+
+private:
+  std::shared_ptr<rclcpp_cascade_lifecycle::CascadeLifecycleNode> node_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr kb_publisher_;
+
+  std::string guest_name_, guest_drink_, guest_description_, guest_id_;
+};
+
+}  // namespace dialog
+
+#endif  // HRI__GEST_GUEST_INFO_HPP_

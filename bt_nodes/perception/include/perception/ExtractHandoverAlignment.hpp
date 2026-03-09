daf759c59012834cea879672a9694ab916b7a37a
Juan Diego <juan97pena@gmail.com>
2026-03-09 11:26:21 +0100

    added nodes for handover and follow person

---
+++ bt_nodes/perception/include/perception/ExtractHandoverAlignment.hpp
@@ -0,0 +1,71 @@
+// Copyright 2024 Intelligent Robotics Lab - Gentlebots
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef PERCEPTION__EXTRACT_HANDOVER_ALIGNMENT_HPP_
+#define PERCEPTION__EXTRACT_HANDOVER_ALIGNMENT_HPP_
+
+#include <string>
+
+#include "behaviortree_cpp_v3/behavior_tree.h"
+#include "behaviortree_cpp_v3/bt_factory.h"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_cascade_lifecycle/rclcpp_cascade_lifecycle.hpp"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+#include "tf2_ros/buffer.h"
+#include "tf2_ros/transform_listener.h"
+#include "yolo_msgs/msg/detection_array.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
+
+namespace perception
+{
+
+class ExtractHandoverAlignment : public BT::ActionNodeBase
+{
+public:
+  explicit ExtractHandoverAlignment(
+    const std::string & xml_tag_name, const BT::NodeConfiguration & conf);
+
+  void halt();
+  BT::NodeStatus tick();
+
+  static BT::PortsList providedPorts()
+  {
+    return BT::PortsList(
+      {BT::InputPort<std::string>("interest_class"),
+       BT::InputPort<double>("target_z_distance"),
+       BT::InputPort<double>("target_y_distance"),
+       BT::OutputPort<double>("base_x_movement"),
+       BT::OutputPort<double>("torso_z_movement")});
+  }
+
+  void detection_callback_(yolo_msgs::msg::DetectionArray::UniquePtr msg);
+  void joint_state_callback_(sensor_msgs::msg::JointState::UniquePtr msg);
+
+private:
+  std::shared_ptr<rclcpp_cascade_lifecycle::CascadeLifecycleNode> node_;
+  rclcpp::Subscription<yolo_msgs::msg::DetectionArray>::SharedPtr detected_objs_sub_;
+  rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_state_sub_;
+  yolo_msgs::msg::DetectionArray::UniquePtr last_detected_objs_ = {nullptr};
+  std::string interest_class_{""};
+  double current_torso_height_ = 0.0;
+  bool torso_height_received_ = false;
+
+  std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+  std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+};
+
+}  // namespace perception
+
+#endif  // PERCEPTION__EXTRACT_HANDOVER_ALIGNMENT_HPP_
